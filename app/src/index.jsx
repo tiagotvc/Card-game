2a938f56772ee379ee5c8f360f5fd129f576321b
Tiago <carvalho110591@gmail.com>
2022-03-25 23:43:48 -0300

    Added store inside index using provider from redux

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './templates/Home';
+import { Provider } from "react-redux";
+import { store } from '../feature/index';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home/>
+    <Provider store={store} >
+      <Home/>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
