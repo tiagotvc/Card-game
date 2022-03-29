eadd57c1a82f638a3a47baf1e26934435e50964e
Tiago <carvalho110591@gmail.com>
2022-03-29 16:35:55 -0300

    Created CarList component

---
+++ app/src/components/CardList/CardList.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Card } from '../Card/Card';
+import * as Styled from './styles';
+
+export const CardList = (props) => {
+    let data = props.data;
+
+    return (
+        <Styled.Container >
+            {data?
+                data.map((card, index) => {
+                    let description = `${card.attributes.canonicalTitle} finished at ${card.attributes.endDate}`;
+                    return(
+                        <li id={index}>
+                            <Card 
+                                image={card.attributes.posterImage.small} 
+                                flipped={props.flipped}
+                                title={card.attributes.canonicalTitle}
+                                description={description}
+                                numb={card.number}
+                            />
+                        </li>
+                    )
+                
+                })
+            : <span>Carregando cartas.....</span>
+            }
+        </Styled.Container>
+    )
+}
---
+++ app/src/components/CardList/styles.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const Container = styled.ul`
+    display:flex;
+    list-style: none ;
+    align-items:center;
+    justify-content:center;
+    padding:0;
+    
+    > li {
+        padding: 2px ;
+    }
+
+    @media(max-width: 1330px){
+        display:grid;
+        grid-template-columns:1fr 1fr 1fr 1fr;
+    }
+
+    @media(max-width: 720px){
+        display:grid;
+        grid-template-columns:1fr 1fr;
+    }
+        
+
+
+
+`
