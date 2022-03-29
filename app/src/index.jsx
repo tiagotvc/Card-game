6f4455e54887a22c2018615d641b0dab4bb4519e
Tiago <carvalho110591@gmail.com>
2022-03-29 16:34:50 -0300

    Changed index.js main to encapsule the route file

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './templates/Home';
 import { Provider } from "react-redux";
-import { store } from '../feature/index';
+import { store } from './feature/index';
+import  AppWrapper  from './routes'
 
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store} >
-      <Home/>
+      <AppWrapper/>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
