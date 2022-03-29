42cbf34a527f12219eecbdf3c10b99a41861f595
Tiago <carvalho110591@gmail.com>
2022-03-29 16:38:34 -0300

    Created card data redux

---
+++ app/src/feature/cardsService/cardsService.js
@@ -0,0 +1,44 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const baseUrl = "https://kitsu.io/api/edge/anime";
+
+export const getCardsData = createAsyncThunk(
+    "cards/getCards",
+    async () => {
+        const response = await axios(`${baseUrl}`);
+        let data = response.data;
+        let numbArray = [...Array(99).keys()];
+
+        const aleatoryNumber = () => { return numbArray[Math.floor(Math.random() * numbArray.length)]; }    
+
+        data.data.map((each) => {
+            each['number'] = aleatoryNumber()
+        })
+    
+        return data;
+    }
+); 
+
+const serviceCards = createSlice({
+    name: "cards",
+    initialState: {
+        cards: [],
+        status:null
+    },
+    extraReducers:{
+        [getCardsData.pending]: (state, action) => {
+            state.status = "loading";
+        },
+        [getCardsData.fulfilled]: (state, action) => {
+            state.status = "sucess";
+            state.cards = action.payload;
+        },
+        [getCardsData.rejected]: (state, action) => {
+            state.status = "failed";
+
+        }
+    }
+})
+
+export default serviceCards.reducer;
