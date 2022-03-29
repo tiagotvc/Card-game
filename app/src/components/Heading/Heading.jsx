f02fefa26bad46fd0286ffadb195ad924b011238
Tiago <carvalho110591@gmail.com>
2022-03-29 16:37:21 -0300

    Created Heading component

---
+++ app/src/components/Heading/Heading.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import P from 'prop-types';
+import * as Styled from './styles';
+
+
+export const Heading = ({
+    size = 'big',
+    uppercase = false,
+    children, 
+    as = 'h1',
+    color = 'black'
+}) => {
+    return (
+        <Styled.Text
+            size={size}
+            uppercase={uppercase}
+            as={as}
+            color={color
+        }>
+            {children}
+        </Styled.Text>
+    );
+};
+
+Heading.propTypes = {
+    children: P.node.isRequired,
+    as: P.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']).isRequired,
+    size: P.oneOf(['small', 'medium', 'big', 'huge']).isRequired,
+    uppercase: P.bool.isRequired,
+    color: P.node.isRequired
+  };
---
+++ app/src/components/Heading/styles.js
@@ -0,0 +1,28 @@
+import styled, { css } from 'styled-components';
+
+const textSize = {
+    small: () => css`
+      font-size: 10px ;
+    `,
+    medium: () => css`
+      font-size: 15px ;
+    `,
+    big: () => css`
+      font-size: 22px ;
+    `,
+    huge: () => css`
+      font-size: 35px ;
+    `,
+  };
+  
+  const textCase = (uppercase) => css`
+    text-transform: ${uppercase ? 'uppercase' : 'none'};
+  `;
+
+export const Text = styled.h1`
+    ${({size, uppercase, color }) => css`
+    color: ${color};
+    ${textSize[size]};
+    ${textCase(uppercase)};
+    `}
+`;
