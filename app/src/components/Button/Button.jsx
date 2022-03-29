be72795839f06e38d78784b55553bd46804df5cb
Tiago <carvalho110591@gmail.com>
2022-03-29 16:24:24 -0300

    Created Button component

---
+++ app/src/components/Button/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import P from 'prop-types';
+import * as Styled from './styles';
+
+
+export const Button = ({children, onClick, disabled}) => {
+
+    return (
+        <Styled.Container disabled={disabled} onClick={onClick}>{children}</Styled.Container>
+    )
+}
+
+Button.propTypes = {
+    children: P.node.isRequired,
+  };
---
+++ app/src/components/Button/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+
+export const Container = styled.button`
+    width:100px;
+    height:50px;
+    border-radius:15px;
+`;
