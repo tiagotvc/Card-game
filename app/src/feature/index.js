eb3460c634802df5e15c36c1a260fa106a24132e
Tiago <carvalho110591@gmail.com>
2022-03-30 17:25:54 -0300

    Created redux hooks to tests

--- app/src/feature/index.js
+++ app/src/feature/index.js
@@ -2,10 +2,12 @@ import serviceCardsReducer from './cardsService/cardsService';
 import serviceNameReducer from './nameService/nameService';
 import { configureStore } from "@reduxjs/toolkit";
 
-
 export const store = configureStore({
     reducer: {
       serviceCards: serviceCardsReducer,
       serviceName: serviceNameReducer
     },
 });
+
+
+
